539af4c7e51c59e3d04b5bb00d48e9a0e85776c2
caoqian <820466903@qq.com>
2019-04-30 12:25:53 +0800

    为资源平台提供定期删除过期视频

---
+++ HResourceDemo/start.bat
@@ -0,0 +1,11 @@
+@echo off
+rem
+rem Add extra JVM options here
+rem
+set OPTS=-Xms256m -Xmx256m
+cd %cd%
+set JAVA_HOME=%cd%\jre8_32
+
+echo %JAVA_HOME%\bin\java.exe
+
+%JAVA_HOME%\bin\java.exe %OPTS% -Djava.ext.dirs=lib;.  com.honghe.resource.Main
